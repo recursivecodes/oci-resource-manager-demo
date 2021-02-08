1fb027286c071039945fd174c8e78399981c0255
recursivecodes <toddraymondsharp@gmail.com>
2021-02-08 16:04:35 -0500

    add variables required by provider

--- demo.tf
+++ demo.tf
@@ -1,8 +1,18 @@
 variable "compartment_ocid" {}
 variable "region" {}
 variable "bucket_name" {}
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key" {}
+variable "private_key_password" {}
 
 provider "oci" {
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid = var.user_ocid
+  fingerprint = var.fingerprint
+  private_key = var.private_key
+  private_key_password = var.private_key_password
   region = var.region
 }
 
@@ -14,7 +24,7 @@ resource "oci_objectstorage_bucket" "create_bucket" {
     compartment_id = var.compartment_ocid
     name = var.bucket_name
     namespace = data.oci_objectstorage_namespace.namespace.namespace
-    access_type = "NoPublicAccess"
+    access_type = "NoPublicAccess" 
 }
 
 output "new_bucket" {

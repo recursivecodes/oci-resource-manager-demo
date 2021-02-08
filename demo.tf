7fd700fd02f66ccae5e3785d663b4f4604339117
recursivecodes <toddraymondsharp@gmail.com>
2021-02-08 13:38:40 -0500

    add schema

--- demo.tf
+++ demo.tf
@@ -1,25 +1,21 @@
-variable "compartment_ocid" {
-  default = "ocid1.compartment.oc1..aaaaaaaa7lzppsdxt6j56zhpvy6u5gyrenwyc2e2h4fak5ydvv6kt7anizbq"
-}
-variable "region" {
-  default = "us-phoenix-1"
-}
-variable "bucket_namespace" {
-    default = "toddrsharp"
-}
-variable "bucket_name" {
-    default = "resource_manager_demo_bucket"
-}
+variable "compartment_ocid" {}
+variable "region" {}
+variable "bucket_namespace" {}
+variable "bucket_name" {}
 
 provider "oci" {
   region = var.region
+}
+
+data "oci_objectstorage_namespace" "namespace" {
+    compartment_id = var.compartment_ocid
 }
 
 resource "oci_objectstorage_bucket" "create_bucket" {
     # required
     compartment_id = var.compartment_ocid
     name = var.bucket_name
-    namespace = var.bucket_namespace
+    namespace = data.oci_objectstorage_namespace.namespace.namespace
 
     # optional
     access_type = "NoPublicAccess" # <---- updated from "ObjectRead"

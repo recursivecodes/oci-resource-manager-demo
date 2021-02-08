15a8227a3fc8acf0d302bd1a7ee32a74e7d48e5c
recursivecodes <toddraymondsharp@gmail.com>
2021-02-08 15:42:53 -0500

    update provider

--- demo.tf
+++ demo.tf
@@ -6,7 +6,8 @@ provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   user_ocid = var.user_ocid
   fingerprint = var.fingerprint
-  private_key_path = var.private_key_path
+  private_key = var.private_key
+  private_key_paswword = var.private_key_paswword
   region = var.region
 }
 

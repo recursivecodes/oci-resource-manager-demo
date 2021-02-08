c418aade3e4282222d1072ed46734be3e485d89c
recursivecodes <toddraymondsharp@gmail.com>
2021-02-08 16:08:16 -0500

    clean up main branch

--- demo.tf
+++ demo.tf
@@ -1,18 +1,8 @@
 variable "compartment_ocid" {}
 variable "region" {}
 variable "bucket_name" {}
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key" {}
-variable "private_key_password" {}
 
 provider "oci" {
-  tenancy_ocid = var.tenancy_ocid
-  user_ocid = var.user_ocid
-  fingerprint = var.fingerprint
-  private_key = var.private_key
-  private_key_password = var.private_key_password
   region = var.region
 }
 
